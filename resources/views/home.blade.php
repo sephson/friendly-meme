16adcfc687d7393f86e6316fea3f2c7c1068c470
sephson <disutj@gmail.com>
2022-02-20 05:25:29 +0100

    added username for auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="col-3">
+      <img src="https://twitter.com/DisuJ0/photo" class="rounded-circle"/>
+  </div>
+  <div class="col-9 pt-5">
+      <div><h1>friendly</h1></div>
+      <div class="d-flex">
+        <div class = "pr-4"> <strong>153</strong> posts </div>
+        <div class = "pr-4"> <strong>10k</strong> followers </div>
+        <div class = "pr-4"> <strong>53</strong> following </div>
+      </div>
+      <div class="pt-4 font-weight-bold">friendly.org</div>
+      <div> we're friendly, we are here for you </div>
+    
+  </div>
 </div>
 @endsection
